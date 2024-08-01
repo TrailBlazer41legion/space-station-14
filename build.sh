6ffee2a2ce084ce40f2b98ba2494307f34dfc2cc
MaxSMokeSkaarj <vnazarov@smokeofanarchy.ru>
2024-08-01 14:06:38 +1000

    Try to formatting rules file, part 10

--- build.sh
+++ build.sh
@@ -45,8 +45,11 @@ function updateSS14() {
 };
 
 function buildServer() {
-	dotnet build Content.Packaging --configuration Release;
-	dotnet run --project Content.Packaging server --hybrid-acz --platform linux-x64;
+	git fetch --depth 1
+	dotnet restore
+	dotnet build Content.Packaging --configuration Release --no-restore /m;
+	dotnet run --project Content.Packaging server --platform win-x64 --platform linux-x64 --platform osx-x64 --platform linux-arm64;
+	dotnet run --project Content.Packaging content --no-wipe-release
 	
 };
 
