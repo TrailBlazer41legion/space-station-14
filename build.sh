05c5d62b89b4705e8261124670e3d2c3099ced32
MaxSMokeSkaarj <vnazarov@smokeofanarchy.ru>
2024-08-01 14:24:21 +1000

    Try to formatting rules file, part 11 and last

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ function buildServer() {
 	dotnet restore
 	dotnet build Content.Packaging --configuration Release --no-restore /m;
 	dotnet run --project Content.Packaging server --platform win-x64 --platform linux-x64 --platform osx-x64 --platform linux-arm64;
-	dotnet run --project Content.Packaging content --no-wipe-release
+	dotnet run --project Content.Packaging client --no-wipe-release
 	
 };
 
