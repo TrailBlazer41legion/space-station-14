3fb28f8daa93fb186e6b84cb05b0c4a74a9c6978
MaxSMokeSkaarj <vnazarov@smokeofanarchy.ru>
2024-11-17 15:36:53 +1000

    Автообновление RobustToolbox

--- update.sh
+++ update.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 export EDITOR="/bin/nano";
 
-function updateCorvaxRepo() {
-	echo "Updating Corvax translations...";
-	cd Corvax-SS14/;
-	git pull origin master;
-	git submodule update --init --recursive;
-	cd ..;
-};
-
-function updateMr0MaksRepo() {
-	echo "Updating Mr0maks translations...";
-	cd Mr0maks-SS14/;
-	git pull origin master;
-	git submodule update --init --recursive;
-	cd ..;
-};
-
 function updateRobustToolbox() {
 	echo "Updating RobustToolbox...";
 	cd RobustToolbox;
@@ -32,15 +16,9 @@ function updateSS14() {
 	updateRobustToolbox
 	git add .;
 	git commit -m "Автообновление RobustToolbox";
-	echo "Updating Server...";
 	git pull origin main;
 	git pull fetch master;
 	git commit -m "Автообновление SS14";
-	updateCorvaxRepo;
-	cp Corvax-SS14/Resources/Locale/ru-RU Resources/Locale -r;
-	cp Corvax-SS14/Resources/ServerInfo/Guidebook Resources/ServerInfo -r;
-	git add .;
-	git commit -m "Автообновление перевода от Corvax";
 	git push origin main;
 	git push --mirror ssh://git@github.com/MaxSMokeSkaarj/space-station-14
 };
